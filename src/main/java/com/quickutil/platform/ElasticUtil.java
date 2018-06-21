d5721219de8ac13e9dfa0a6b41795934f9648b9d
sean.jie <sean.jie>
2018-06-21 18:36:09 +0800

    remove junit pom and import *

--- src/main/java/com/quickutil/platform/ElasticUtil.java
+++ src/main/java/com/quickutil/platform/ElasticUtil.java
@@ -10,12 +10,26 @@ import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.function.Function;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLHandshakeException;
 
-import org.apache.http.*;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.TruncatedChunkException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
@@ -86,9 +100,8 @@ public class ElasticUtil {
 			}
 			HttpClientContext clientContext = HttpClientContext.adapt(context);
 			HttpRequest request = clientContext.getRequest();
-			if (!(request instanceof HttpEntityEnclosingRequest)) { // 如果请求是幂等的，就再次尝试
-				return true;
-			}
+            // 如果请求是幂等的，就再次尝试
+            if (!(request instanceof HttpEntityEnclosingRequest)) return true;
 			return false;
 		}
 	};
