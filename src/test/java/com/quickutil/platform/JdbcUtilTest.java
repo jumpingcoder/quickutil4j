49078f473f83ca6859f4b85012a810f4959080b0
message.miao <message.miao@dji.com>
2020-11-25 09:59:39 +0800

    1.6.9

---
+++ src/test/java/com/quickutil/platform/JdbcUtilTest.java
@@ -0,0 +1,22 @@
+package com.quickutil.platform;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * 关系型数据库Jdbc工具
+ *
+ * @author 0.5
+ */
+
+public class JdbcUtilTest {
+    @Test
+    public void convertType() {
+        Assert.assertEquals("aaa_bbb_ccc", JdbcUtil.convertColumnName("aaa_bbb_ccc", JdbcUtil.ConvertEnum.NO_COVERT));
+        Assert.assertEquals("aaaBbbCcc", JdbcUtil.convertColumnName("aaa_bbb_ccc", JdbcUtil.ConvertEnum.SNACK_TO_SMALL_CAMEL));
+        Assert.assertEquals("AaaBbbCcc", JdbcUtil.convertColumnName("aaa_bbb_ccc", JdbcUtil.ConvertEnum.SNACK_TO_BIG_CAMEL));
+        Assert.assertEquals("aaaBbbCcc", JdbcUtil.convertColumnName("aaa_bbb_ccc_", JdbcUtil.ConvertEnum.SNACK_TO_SMALL_CAMEL));
+        Assert.assertEquals("", JdbcUtil.convertColumnName("", JdbcUtil.ConvertEnum.SNACK_TO_SMALL_CAMEL));
+    }
+}
