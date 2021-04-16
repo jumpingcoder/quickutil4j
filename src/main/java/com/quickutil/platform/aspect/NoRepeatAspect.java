837c93445bc27722b62fab64578f00475852783e
dji <dji@djideMacBook-Pro.local>
2021-04-16 19:50:58 +0800

    no repeat aspect 3

--- src/main/java/com/quickutil/platform/aspect/NoRepeatAspect.java
+++ src/main/java/com/quickutil/platform/aspect/NoRepeatAspect.java
@@ -2,29 +2,27 @@ package com.quickutil.platform.aspect;
 
 import com.quickutil.platform.EnvironmentUtil;
 import com.quickutil.platform.exception.NoRepeatException;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.params.SetParams;
 
 @Aspect
-@Component
 public class NoRepeatAspect implements Ordered {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(NoRepeatAspect.class);
-	private static JedisPool jedisPool = null;
+	private JedisPool jedisPool = null;
 
-	public static void setJedisPool(JedisPool pool) {
-		jedisPool = pool;
+	public NoRepeatAspect(JedisPool jedisPool) {
+		this.jedisPool = jedisPool;
 	}
 
 	@Override
@@ -32,21 +30,21 @@ public class NoRepeatAspect implements Ordered {
 		return 1;
 	}
 
-	@Pointcut("@annotation(notRepeat)")
-	public void pointCut(NoRepeat notRepeat) {
+	@Pointcut("@annotation(noRepeat)")
+	public void pointCut(NoRepeat noRepeat) {
 	}
 
-	@Around("pointCut(notRepeat)")
-	public Object around(ProceedingJoinPoint point, NoRepeat notRepeat) throws Throwable {
+	@Around("pointCut(noRepeat)")
+	public Object around(ProceedingJoinPoint point, NoRepeat noRepeat) throws Throwable {
 		LOGGER.debug("NoRepeat around");
 		if (jedisPool == null) {
-			throw new NullPointerException("NotRepeat need set jedis pool before use");
+			throw new NullPointerException("NoRepeat need set jedis pool before use");
 		}
 		String keyName = String.format("nopeat:%s:%s", point.getSignature().getDeclaringTypeName(), point.getSignature().getName());
 		String value = EnvironmentUtil.getMachineFinger() + ":" + System.currentTimeMillis();
 		Jedis jedis = jedisPool.getResource();
 		try {
-			String result = jedis.set(keyName, value, new SetParams().nx().ex(notRepeat.lockExpire()));
+			String result = jedis.set(keyName, value, new SetParams().nx().ex(noRepeat.lockExpire()));
 			if (!"OK".equals(result)) {
 				throw new NoRepeatException(keyName + " is running");
 			}
@@ -58,14 +56,17 @@ public class NoRepeatAspect implements Ordered {
 		return point.proceed();
 	}
 
-	@Before("pointCut(notRepeat)")
-	public void before(NoRepeat notRepeat) {
-		LOGGER.debug("NoRepeat before");
-	}
-
-	@After("pointCut(notRepeat)")
-	public void After(NoRepeat notRepeat) {
+	@After("pointCut(noRepeat)")
+	public void After(JoinPoint point, NoRepeat noRepeat) {
 		LOGGER.debug("NoRepeat after");
+		String keyName = String.format("nopeat:%s:%s", point.getSignature().getDeclaringTypeName(), point.getSignature().getName());
+		Jedis jedis = jedisPool.getResource();
+		try {
+			jedis.del(keyName);
+		} finally {
+			if (jedis != null) {
+				jedis.close();
+			}
+		}
 	}
-
 }
